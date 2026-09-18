7c372305945f3a5a4e1d5c1f3ca4111705db1fba
StendaH <edwardson.pascual@gmail.com>
2026-09-18 12:25:50 -0400

    Changed /serverinfo to post an embed

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require( 'discord.js' );
+
 /**
  * 
  * This command retrieves information about the server (guild) where the command is executed.
@@ -8,7 +10,7 @@
  *   guild: import( 'discord.js' ).Guild,
  *   reply: Function
  * 
- * } - interaction.
+ * } - Interaction.
  * 
  */
 
@@ -20,24 +22,44 @@ async function serverinfo( interaction ) {
     // Get the member count of the guild
     const memberCount = guild.memberCount;
 
-    // Get the name of the guild
-    const guildName = guild.name;
-
-    // Get the ID of the guild
-    const guildId = guild.id;
-
     // Get the creation date of the guild
     const creationDate = guild.createdAt;
 
     // Get the owner of the guild
     const owner = await guild.fetchOwner();
 
+    // Get the number of channels and roles
+    const channelCount = guild.channels.cache.size;
+    const roleCount = guild.roles.cache.size;
+
+    /*
     // Create a response message with the server information
-    const responseMessage = `Server Name: ${ guildName }\nServer ID: ${ guildId }\nMember Count: ${ memberCount }\nCreation Date: ${ creationDate.toDateString() }\nOwner: ${ owner.user.tag }`;
+    const responseMessage = `Server Name: ${ guildName }\nServer ID: ${ guildId }\nMember Count: ${ memberCount }\nCreation Date: ${ creationDate.toDateString() }\nOwner: ${ owner.user.tag }\nChannel Count: ${ channelCount }\nRole Count: ${ roleCount }`;
+    */
+
+    // Embed the server information in a visually appealing format
+    const serverInfoEmbed = new EmbedBuilder()
+    
+        .setTitle( guild.name )
+        .setThumbnail( guild.iconURL() )
+        .setColor( 0x5865F2 ) // Discord's own "blurple" brand color
+        .addFields(
+
+            { name: 'Member Count', value: `${ memberCount }`, inline: true },
+            { name: 'Creation Date', value: creationDate.toDateString(), inline: true },
+            { name: 'Owner', value: owner.user.tag, inline: true },
+            { name: 'Channels', value: `${ channelCount }`, inline: true },
+            { name: 'Roles', value: `${ roleCount }`, inline: true }
+
+        );
 
     // Reply to the interaction with the server information
-    await interaction.reply( responseMessage );
+    await interaction.reply( { embeds: [serverInfoEmbed] } );
 
 }
 
-module.exports = { name: 'serverinfo', execute: serverinfo };
+module.exports = {
+    
+    name: 'serverinfo', execute: serverinfo
+
+};
